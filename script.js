a4a2ff91f6f89254758291aa4ef0441794b1361f
manish-gitx <108692670+manish-gitx@users.noreply.github.com>
2023-12-05 18:11:14 +0530

    final

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-const url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=574474fa2c17443ba48b249160bf0800";
+const baseurl="https://newsapi.org/v2";
+const key="&apiKey=574474fa2c17443ba48b249160bf0800";
 const cards=document.querySelector(".cards");
-
+const category=document.querySelector(".category");
+const span=document.querySelectorAll(".category span");
+var image="https://media.istockphoto.com/id/503149471/photo/blank-daily-newspaper.jpg?s=612x612&w=0&k=20&c=fdeuNjXj7YRdPSFVQWciJBsAO8sriI2H5dWsVSl8gQw=";
 
 
 async function request(url){
@@ -15,11 +18,13 @@ async function request(url){
 
 }
 // const data=request(url).then(manish=>console.log(manish))
+
+function getall(url){
 request(url).then(data=>{
     data.articles.forEach( post=> {
         cards.innerHTML +=`                    <div class="card">
         <div class="image">
-            <img src="${post.urlToImage}" alt="Default News Image">
+            <img src="${post.urlToImage ? post.urlToImage :image}" alt="Default News Image">
         </div>
         <div class="information">
             <div>
@@ -39,3 +44,18 @@ request(url).then(data=>{
         
     });
 })
+}
+category.addEventListener('click',(event)=>{
+    if (event.target.tagName === "SPAN"){
+        cards.innerHTML="";
+        span.forEach((item) => {
+            item.classList.remove("active")
+        });
+        event.target.classList.add("active"); 
+        const link=event.target.dataset.id;
+        getall(baseurl+link+key);
+    }
+})
+
+
+getall(baseurl+"/top-headlines?country=us&category=business"+key)
